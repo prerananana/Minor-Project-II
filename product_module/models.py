ed3992149606ce90b229451e6b98b9138ddfdb07
Prerana Mali <prerana.mali@apexcollege.edu.np>
2022-08-02 16:42:05 +0545

    aa

--- product_module/models.py
+++ product_module/models.py
@@ -1,4 +1,4 @@
-from importlib.resources import Package
+# from importlib.resources import Package as auth_Package
 from django.db import models
 from django.utils.html import mark_safe
 from django.contrib.auth.models import User
@@ -10,14 +10,14 @@ from django.contrib.auth.models import User
 #     is_customer = models.BooleanField('Is customer', default=False)
 #     is_agency = models.BooleanField('Is agency', default=False)
 
-class customer(models.Model):
+class Customer(models.Model):
     customer_id= models.IntegerField()
     fullname= models.CharField(max_length=200)
     address= models.CharField(max_length=200)
     phone= models.CharField(max_length=200)
     email= models.CharField(max_length=300)
 
-class guide(models.Model):
+class Guide(models.Model):
     guide_id= models.IntegerField()
     guide_name= models.CharField(max_length=200)
     experience= models.IntegerField()
@@ -26,7 +26,7 @@ class guide(models.Model):
     no_of_routes= models.IntegerField()
     review= models.CharField(max_length=500)
 
-class package(models.Model):
+class Package(models.Model):
     package_id= models.IntegerField()
     package_name= models.CharField(max_length=200)
     price= models.FloatField()
@@ -37,21 +37,21 @@ class package(models.Model):
     time= models.CharField(max_length=100, default="5 days")
     package_name_alias= models.CharField(max_length=100, default="")
 
-class agency(models.Model):
+class Agency(models.Model):
     agency_id= models.IntegerField()
     agency_name= models.CharField(max_length=200)
     location= models.CharField(max_length=200)
-    package= models.ForeignKey(package, on_delete=models.CASCADE)
+    package= models.ForeignKey(Package, on_delete=models.CASCADE)
     class Meta:
         verbose_name_plural = "Agencies"
 
-class booking(models.Model):
+class Booking(models.Model):
     booking_id= models.IntegerField()
     destination= models.CharField(max_length=200)
     ticket_type= models.CharField(max_length=200)
-    customer= models.ForeignKey(customer, on_delete=models.CASCADE, null=True)
-    guide= models.ForeignKey(guide, on_delete=models.CASCADE)
-    package= models.ForeignKey(package, on_delete=models.CASCADE)
+    customer= models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
+    guide= models.ForeignKey(Guide, on_delete=models.CASCADE)
+    package= models.ForeignKey(Package, on_delete=models.CASCADE)
     # destination_detail= models.ForeignKey(destination_detail, on_delete=models.CASCADE)
 
 class destination_detail(models.Model):
@@ -59,10 +59,10 @@ class destination_detail(models.Model):
     destination= models.CharField(max_length=200)
     description= models.TextField(blank= True)
     days= models.TextField(blank=True, default="")
-    package= models.ForeignKey(package, on_delete=models.CASCADE)
+    package= models.ForeignKey(Package, on_delete=models.CASCADE)
 
 class CartItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default="")
-    package = models.ForeignKey(package, on_delete=models.CASCADE)
+    package = models.ForeignKey(Package, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     entered_on = models.DateTimeField()
